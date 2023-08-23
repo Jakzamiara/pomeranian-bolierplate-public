74d10ecad9276904238c08c47349d9e36063dc0b
Jakzamiara <jakzamiara@gmail.com>
2023-08-23 20:13:14 +0200

    AppAside

--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -1,24 +1,27 @@
-import { NavLink } from 'react-router-dom';
-
-import './styles/aside.css';
+import { NavLink } from "react-router-dom";
+import { HouseIcon } from "../Components/Icons/HouseIcon";
+import { ElementIcon } from "../Components/Icons/ElementIcon";
+import "./styles/aside.css";
 
 export function AppAside() {
   return (
     <aside>
       <nav>
         <ul>
-          <li>
+          <li className="aside-row">
+            <HouseIcon />
             <NavLink to="dashboard">Dashboard</NavLink>
           </li>
-          <li>
+          <li className="aside-row">
+            <ElementIcon />
             <NavLink to="blocks">Bloki</NavLink>
           </li>
-          <li>
+          <li className="aside-row">
             <NavLink to="exercises">Ćwiczenia</NavLink>
           </li>
         </ul>
       </nav>
-      <p style={{ padding: '1rem 0' }}>Sidebar items, widgets, etc</p>
+      <p style={{ padding: "1rem 0" }}>Sidebar items, widgets, etc</p>
     </aside>
   );
 }
