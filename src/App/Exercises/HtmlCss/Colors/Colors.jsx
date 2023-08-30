574f31a8e1daf7e7b1ff3945cb82485f0bd97919
Jakzamiara <jakzamiara@gmail.com>
2023-08-30 18:52:36 +0200

    colors

--- src/App/Exercises/HtmlCss/Colors/Colors.jsx
+++ src/App/Exercises/HtmlCss/Colors/Colors.jsx
@@ -1,12 +1,19 @@
 import "./styles.css";
 export const Colors = () => {
   return (
-    <div>
-      <h2 className="colorName">Jestem pomalowany przy użyciu "colorName"</h2>
-      <h2 className="shortenHex">Jestem pomalowany przy użyciu "shortenHex"</h2>
-      <h2 className="hex">Jestem pomalowany przy użyciu "hex"</h2>
-      <h2 className="rgb">Jestem pomalowany przy użyciu "rgb"</h2>
-      <h2 className="rgba">Jestem pomalowany przy użyciu "rgba"</h2>
-    </div>
+    <>
+      <div>
+        <h2 className="colorName">Jestem pomalowany przy użyciu "colorName"</h2>
+        <h2 className="shortenHex">
+          Jestem pomalowany przy użyciu "shortenHex"
+        </h2>
+        <h2 className="hex">Jestem pomalowany przy użyciu "hex"</h2>
+        <h2 className="rgb">Jestem pomalowany przy użyciu "rgb"</h2>
+        <h2 className="rgba">Jestem pomalowany przy użyciu "rgba"</h2>
+      </div>
+      <hr />
+      <div className="opacity">Jestem divem z teksem</div>
+      <div className="rgbaopacity">Jestem divem z teksem</div>
+    </>
   );
 };
