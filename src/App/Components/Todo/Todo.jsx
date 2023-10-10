fbd5b62c23eb5969de6f8c2b4391631a8b264bc2
Jakzamiara <jakzamiara@gmail.com>
2023-10-11 01:41:39 +0200

    Todotwo

--- src/App/Components/Todo/Todo.jsx
+++ src/App/Components/Todo/Todo.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
-import { Todoelement } from "./Todo-element";
+import { Todoelement } from "./Todoelement";
 import { Todoadd } from "./Todo-add";
 
 export const Todo = () => {
